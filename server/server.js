5a63d54559ce1375d8e7113e782de8da63df9fbe
JHoang <JHoang6900@gmail.com>
2023-03-12 00:21:01 -0700

    changed to dist

--- server/server.js
+++ server/server.js
@@ -37,7 +37,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, "../client/build")));
 
 app.get('/*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+  res.sendFile(path.join(__dirname, '../client/dist/index.html'));
 });
 
 // Create a new instance of an Apollo server with the GraphQL schema
